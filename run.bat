048feeb0a0ad93c34234f98cad3eefe61131706e
Lucifer <knitin.gawande95@gmail.com>
2024-10-21 19:21:32 +0530

    removed alluere

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 pytest -s -v TestCases\loginpage.py
 REM pytest -s -v --alluredir="C:\Users\Anshu\Desktop\New folder (3)\automation testing notes\project\orangehrm\allurereport\reports" allurereport\test_orange.py
 REM allure serve "C:\Users\Anshu\Desktop\New folder (3)\automation testing notes\project\orangehrm\allure-results"
-pytest -s -v allurereport\test_orange.py
+REM pytest -s -v allurereport\test_orange.py
+REM pytest -s -v allurereport\test_orange.py  
